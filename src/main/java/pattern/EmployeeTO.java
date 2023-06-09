70acd64b152278dc2e59ec58bb8faae97648ae4e
baeyuyeon <dbdus983@naver.com>
2023-06-09 16:07:45 +0900

    Initial commit

---
+++ src/main/java/pattern/EmployeeTO.java
@@ -0,0 +1,50 @@
+package pattern;
+
+import java.io.Serializable;
+
+public class EmployeeTO implements Serializable {
+    private String empName;
+    private String empID;
+    private String empPhone;
+    public EmployeeTO(){
+        super();
+    }
+
+    public EmployeeTO(String empName, String empID, String empPhone) {
+        this.empName = empName;
+        this.empID = empID;
+        this.empPhone = empPhone;
+    }
+
+    public String getEmpName() {
+        if(empName == null) return "";
+        return empName;
+    }
+
+    public void setEmpName(String empName) {
+        this.empName = empName;
+    }
+
+    public String getEmpID() {
+        return empID;
+    }
+
+    public void setEmpID(String empID) {
+        this.empID = empID;
+    }
+
+    public String getEmpPhone() {
+        return empPhone;
+    }
+
+    public void setEmpPhone(String empPhone) {
+        this.empPhone = empPhone;
+    }
+
+    public String toString(){
+        StringBuilder sb  = new StringBuilder();
+        sb.append("empName=").append(empName).append("empId=")
+                .append(empID).append(" empPhone=").append(empPhone);
+        return sb.toString();
+    }
+}
